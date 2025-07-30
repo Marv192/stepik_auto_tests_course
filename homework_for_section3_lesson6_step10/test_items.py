5e8a7f5d871f17982fa88afb064f2a6460eae522
Marv192 <marvel.92@inbox.ru>
2025-07-30 16:06:40 +0300

    fixed script

--- homework_for_section3_lesson6_step10/test_items.py
+++ homework_for_section3_lesson6_step10/test_items.py
@@ -1,39 +1,10 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as OptionsFirefox
+import time
+from selenium.webdriver.common.by import By
 
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    assert browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket'), "No add button"
+    time.sleep(20)
 
-    parser.addoption('--language', action='store', default=None, help='Choose language: ru, en, ...')
-
-
-@pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("language")
-
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option(
-        'prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-        browser.implicitly_wait(5)
-
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        options_firefox = OptionsFirefox()
-        options_firefox.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(options=options_firefox)
-        browser.implicitly_wait(5)
-
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
